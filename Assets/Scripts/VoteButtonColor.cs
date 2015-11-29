9afc56fc4566457b64c1ed0056f1f78cc47820f5
Badim150 <cm_margarido@hotmail.com>
2015-11-29 18:19:52 +0000

    Button
    Increments and goes green if pressed.
    Falta: fazer o vote unique

---
+++ Assets/Scripts/VoteButtonColor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VoteButtonColor : MonoBehaviour {
+    int color = 0;
+    public void ButtonPressed()
+    {
+        
+       Button b = GetComponent<Button>();
+        if (color==0)
+        {
+            b.image.color = Color.green;
+            color = 1;
+        }
+        else {
+            color = 0;
+            b.image.color = Color.white;
+        }
+    }
+	
+}
---
+++ Assets/Scripts/VoteIncrement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VoteIncrement : MonoBehaviour {
+    int Vote = 0;
+    Text instruction;
+    public void UpVote()
+    {
+        instruction = GetComponent<Text>();
+
+        if (Vote == 0)
+        {
+            instruction.text = (int.Parse(instruction.text) + 1).ToString();
+            Vote = 1;
+        }
+        else
+        {
+            instruction.text = (int.Parse(instruction.text) - 1).ToString();
+            Vote = 0;
+        }
+    }
+
+}
+
