d04dbc7a27e4120646c5eb55ee3a2564a4f665ba
Badim150 <cm_margarido@hotmail.com>
2015-11-29 18:33:59 +0000

    HasVoted
    Implemented

--- Assets/Scripts/VoteButtonColor.cs
+++ Assets/Scripts/VoteButtonColor.cs
@@ -5,17 +5,21 @@ using System.Collections;
 
 public class VoteButtonColor : MonoBehaviour {
     int color = 0;
+    static int HasVoted = 0;
     public void ButtonPressed()
     {
-        
+      
        Button b = GetComponent<Button>();
-        if (color==0)
+        if (color==0 && HasVoted == 0)
         {
             b.image.color = Color.green;
             color = 1;
+            HasVoted = 1;
         }
-        else {
+        else if(color == 1 && HasVoted == 1)
+        {
             color = 0;
+            HasVoted = 0;
             b.image.color = Color.white;
         }
     }
